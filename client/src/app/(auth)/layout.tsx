53b918fbc64678fc824477ac791eead9eae54674
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-27 03:12:45 +0500

    style(ui): increase lightning effect size to 1.2

--- client/src/app/(auth)/layout.tsx
+++ client/src/app/(auth)/layout.tsx
@@ -8,7 +8,13 @@ export default function AuthLayout({
   return (
     <section className="relative h-screen w-full overflow-hidden">
       <div className="absolute inset-0 -z-10">
-        <Lightning hue={260} xOffset={-0.1} speed={1} intensity={2} size={1} />
+        <Lightning
+          hue={260}
+          xOffset={-0.1}
+          speed={1}
+          intensity={2}
+          size={1.2}
+        />
       </div>
 
       <div className="relative flex h-full w-full flex-col items-center justify-center">
